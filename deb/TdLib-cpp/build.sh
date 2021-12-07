771fcfdb5f575d0660abc9366655ad18858282d1
Animesh Murmu <am2646374@gmail.com>
2021-12-07 21:15:48 +0530

    Update: 1.7.10

--- deb/TdLib-cpp/build.sh
+++ deb/TdLib-cpp/build.sh
@@ -4,8 +4,12 @@ pkg install clang --upgrade
 pkg install git make cmake zlib openssl php build-essential binutils boost
 if [ ! -d "td" ]; then
     git clone https://github.com/tdlib/td.git
+    cd td
+else
+    cd td
+    git pull
 fi
-cd td
+
 rm build -rf
 mkdir build
 cd build
