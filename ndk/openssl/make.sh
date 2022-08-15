5c5ad21ad006264a62f20126baaa15e69acc6325
Animesh Murmu <47624427+animeshxd@users.noreply.github.com>
2022-08-15 15:39:18 +0530

    Update make.sh

--- ndk/openssl/make.sh
+++ ndk/openssl/make.sh
@@ -2,7 +2,9 @@ ABI=x86_64 # arm64-v8a # https://developer.android.com/ndk/guides/cmake#android_
 
 API_LEVEL=21
 ANDROID_NDK_BUNDLE=/home/user/.local/opt/Android/ndk-bundle
+INSTALLDIR=/home/user/.local/opt/Android/ndk-builds/$API_LEVEL/$ABI/
+
 export ANDROID_NDK_ROOT=/home/user/.local/opt/Android/ndk/25.0.8775105/
 export PATH=$ANDROID_NDK_ROOT/toolchains/llvm/prebuilt/linux-x86_64/bin:$ANDROID_NDK_BUNDLE/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin:$PATH
-./Configure android-$ABI -D__ANDROID_API__=$API_LEVEL --prefix=/home/user/.local/opt/Android/ndk-builds-$API_LEVEL/$ABI/
+./Configure android-$ABI -D__ANDROID_API__=$API_LEVEL --prefix=$INSTALLDIR
 make
